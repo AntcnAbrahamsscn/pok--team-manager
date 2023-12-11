d21d6dc676c91e17a03930c586051f51f527ba8c
Anton Abrahamsson <abrahamsson37@gmail.com>
2023-12-11 09:33:07 +0100

    Initial commit

---
+++ themecolor.js
@@ -0,0 +1,51 @@
+const redButton = document.querySelector("#red-button")
+const yellowButton = document.querySelector("#yellow-button")
+const blueButton = document.querySelector("#blue-button")
+const colorSectionContainer = document.querySelector(".color-selection-container")
+const pokemonImgFP = document.querySelector("#pokemon-image-fp")
+const h1Fp = document.querySelector("#h1-fp")
+const h2Fp = document.querySelector("#h2-fp")
+const startButton = document.querySelector(".start-button")
+
+
+
+redButton.addEventListener("click", () => {
+    colorSectionContainer.classList.add("background-color-red");
+    colorSectionContainer.classList.remove("background-color-yellow");
+    colorSectionContainer.classList.remove("background-color-blue");
+    h1Fp.classList.add("background-color-red");
+    h1Fp.classList.remove("background-color-yellow");
+    h1Fp.classList.remove("background-color-blue");
+    h2Fp.classList.add("background-color-red");
+    h2Fp.classList.remove("background-color-yellow");
+    h2Fp.classList.remove("background-color-blue");
+    pokemonImgFP.src = "./assets/img/pokemon-red.png";
+});
+
+yellowButton.addEventListener("click", () => {
+    colorSectionContainer.classList.add("background-color-yellow");
+    colorSectionContainer.classList.remove("background-color-red");
+    colorSectionContainer.classList.remove("background-color-blue");
+    h1Fp.classList.add("background-color-yellow");
+    h1Fp.classList.remove("background-color-blue");
+    h1Fp.classList.remove("background-color-red");
+    h2Fp.classList.add("background-color-yellow");
+    h2Fp.classList.remove("background-color-blue");
+    h2Fp.classList.remove("background-color-red");
+    pokemonImgFP.src = "./assets/img/pokemon-yellow.png"; 
+});
+
+blueButton.addEventListener("click", () => {
+    colorSectionContainer.classList.add("background-color-blue");
+    colorSectionContainer.classList.remove("background-color-yellow");
+    colorSectionContainer.classList.remove("background-color-red");
+    h1Fp.classList.add("background-color-blue");
+    h1Fp.classList.remove("background-color-red");
+    h1Fp.classList.remove("background-color-yellow");
+    h2Fp.classList.add("background-color-blue");
+    h2Fp.classList.remove("background-color-red");
+    h2Fp.classList.remove("background-color-yellow");
+    pokemonImgFP.src = "./assets/img/pokemon-blue.png";
+});
+
+
