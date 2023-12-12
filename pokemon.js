7cccc7ba0709646239b1643ba58e71187abdf4ee
Anton Abrahamsson <abrahamsson37@gmail.com>
2023-12-12 15:15:44 +0100

    Fixed transitions for buttons

--- pokemon.js
+++ pokemon.js
@@ -1,12 +1,7 @@
 const pokemonSearchInput = document.querySelector("#pokemon-search-input");
 const pokemonSearchButton = document.querySelector("#pokemon-search-button");
 
-// Remove the click event listener on the input field
-// pokemonSearchInput.addEventListener("click", (event) => {
-//     event.preventDefault();
-// });
-
 pokemonSearchButton.addEventListener("click", (event) => {
-    event.preventDefault();  // Prevent form submission or other default actions
+    event.preventDefault();
     console.log(pokemonSearchInput.value);
 });
--- themecolor.js
+++ themecolor.js
@@ -1,10 +1,10 @@
-const redButton = document.querySelector("#red-button")
-const yellowButton = document.querySelector("#yellow-button")
-const blueButton = document.querySelector("#blue-button")
-const colorSectionContainer = document.querySelector(".color-selection-container")
-const pokemonImgFP = document.querySelector("#pokemon-image-fp")
-const h1Fp = document.querySelector("#h1-fp")
-const h2Fp = document.querySelector("#h2-fp")
+// const redButton = document.querySelector("#red-button")
+// const yellowButton = document.querySelector("#yellow-button")
+// const blueButton = document.querySelector("#blue-button")
+// const colorSectionContainer = document.querySelector(".color-selection-container")
+// const pokemonImgFP = document.querySelector("#pokemon-image-fp")
+// const h1Fp = document.querySelector("#h1-fp")
+// const h2Fp = document.querySelector("#h2-fp")
 const startButton = document.querySelector(".start-button")
 const firstPageContainer = document.querySelector(".first-page-flex-container")
 const teamManagerPage = document.querySelector("#team-manager-page")
@@ -12,58 +12,37 @@ const circleMenu = document.querySelector(".circle-menu")
 const teamNameInput = document.querySelector(".team-name-input")
 const teamName = document.querySelector(".team-name")
 
+const themeButtons = document.querySelectorAll('.theme-btn');
+    const h1Fp = document.querySelector('.h1-fp');
+    const h2Fp = document.querySelector(".h2-fp");
+    const colorSelectionContainer = document.querySelector(".color-selection-container");
 
+function changePokemonImg(theme) {
+    const pokemonImage = document.getElementById('pokemon-image-fp');
+    if (theme === 'red') {
+        pokemonImage.src = './assets/img/pokemon-red.png';
+    } else if (theme === 'blue') {
+        pokemonImage.src = './assets/img/pokemon-blue.png';
+    } else if (theme === 'yellow') {
+        pokemonImage.src = './assets/img/pokemon-yellow.png';
+    }
+}
 
-redButton.addEventListener("click", () => {
-    colorSectionContainer.classList.add("background-color-red");
-    colorSectionContainer.classList.remove("background-color-yellow");
-    colorSectionContainer.classList.remove("background-color-blue");
-    h1Fp.classList.add("background-color-red");
-    h1Fp.classList.remove("background-color-yellow");
-    h1Fp.classList.remove("background-color-blue");
-    h2Fp.classList.add("background-color-red");
-    h2Fp.classList.remove("background-color-yellow");
-    h2Fp.classList.remove("background-color-blue");
-    circleMenu.classList.add("background-color-red")
-    circleMenu.classList.remove("background-color-blue")
-    circleMenu.classList.remove("background-color-yellow")
-    // teamName.classList.add("background-color-red")
-    // teamName.classList.remove("background-color-blue")
-    // teamName.classList.remove("background-color-yellow")
-    pokemonImgFP.src = "./assets/img/pokemon-red.png";
+themeButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        const theme = this.dataset.theme;
+        h1Fp.classList.remove('theme-red', 'theme-blue', 'theme-yellow');
+        h1Fp.classList.add(`theme-${theme}`);
+        h2Fp.classList.remove('theme-red', 'theme-blue', 'theme-yellow');
+        h2Fp.classList.add(`theme-${theme}`);
+        colorSelectionContainer.classList.remove('theme-red', 'theme-blue', 'theme-yellow');
+        colorSelectionContainer.classList.add(`theme-${theme}`);
+        changePokemonImg(theme)
+    });
 });
 
-yellowButton.addEventListener("click", () => {
-    colorSectionContainer.classList.add("background-color-yellow");
-    colorSectionContainer.classList.remove("background-color-red");
-    colorSectionContainer.classList.remove("background-color-blue");
-    h1Fp.classList.add("background-color-yellow");
-    h1Fp.classList.remove("background-color-blue");
-    h1Fp.classList.remove("background-color-red");
-    h2Fp.classList.add("background-color-yellow");
-    h2Fp.classList.remove("background-color-blue");
-    h2Fp.classList.remove("background-color-red");
-    circleMenu.classList.add("background-color-yellow")
-    circleMenu.classList.remove("background-color-red")
-    circleMenu.classList.remove("background-color-blue")
-    pokemonImgFP.src = "./assets/img/pokemon-yellow.png"; 
-});
 
-blueButton.addEventListener("click", () => {
-    colorSectionContainer.classList.add("background-color-blue");
-    colorSectionContainer.classList.remove("background-color-yellow");
-    colorSectionContainer.classList.remove("background-color-red");
-    h1Fp.classList.add("background-color-blue");
-    h1Fp.classList.remove("background-color-red");
-    h1Fp.classList.remove("background-color-yellow");
-    h2Fp.classList.add("background-color-blue");
-    h2Fp.classList.remove("background-color-red");
-    h2Fp.classList.remove("background-color-yellow");
-    circleMenu.classList.add("background-color-blue")
-    circleMenu.classList.remove("background-color-red")
-    circleMenu.classList.remove("background-color-yellow")
-    pokemonImgFP.src = "./assets/img/pokemon-blue.png";
-});
+
 
 startButton.addEventListener("click", (event)=> {
     event.preventDefault();
