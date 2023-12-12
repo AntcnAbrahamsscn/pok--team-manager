158d9d2b43dcac082ce7fb43095332ec5d975dbd
Anton Abrahamsson <abrahamsson37@gmail.com>
2023-12-12 10:52:36 +0100

    Added new features to second page

--- themecolor.js
+++ themecolor.js
@@ -7,8 +7,10 @@ const h1Fp = document.querySelector("#h1-fp")
 const h2Fp = document.querySelector("#h2-fp")
 const startButton = document.querySelector(".start-button")
 const firstPageContainer = document.querySelector(".first-page-flex-container")
-const searchField = document.querySelector(".search")
 const teamManagerPage = document.querySelector("#team-manager-page")
+const circleMenu = document.querySelector(".circle-menu")
+const teamNameInput = document.querySelector(".team-name-input")
+const teamName = document.querySelector(".team-name")
 
 
 
@@ -22,6 +24,12 @@ redButton.addEventListener("click", () => {
     h2Fp.classList.add("background-color-red");
     h2Fp.classList.remove("background-color-yellow");
     h2Fp.classList.remove("background-color-blue");
+    circleMenu.classList.add("background-color-red")
+    circleMenu.classList.remove("background-color-blue")
+    circleMenu.classList.remove("background-color-yellow")
+    // teamName.classList.add("background-color-red")
+    // teamName.classList.remove("background-color-blue")
+    // teamName.classList.remove("background-color-yellow")
     pokemonImgFP.src = "./assets/img/pokemon-red.png";
 });
 
@@ -35,6 +43,9 @@ yellowButton.addEventListener("click", () => {
     h2Fp.classList.add("background-color-yellow");
     h2Fp.classList.remove("background-color-blue");
     h2Fp.classList.remove("background-color-red");
+    circleMenu.classList.add("background-color-yellow")
+    circleMenu.classList.remove("background-color-red")
+    circleMenu.classList.remove("background-color-blue")
     pokemonImgFP.src = "./assets/img/pokemon-yellow.png"; 
 });
 
@@ -48,13 +59,24 @@ blueButton.addEventListener("click", () => {
     h2Fp.classList.add("background-color-blue");
     h2Fp.classList.remove("background-color-red");
     h2Fp.classList.remove("background-color-yellow");
+    circleMenu.classList.add("background-color-blue")
+    circleMenu.classList.remove("background-color-red")
+    circleMenu.classList.remove("background-color-yellow")
     pokemonImgFP.src = "./assets/img/pokemon-blue.png";
 });
 
-startButton.addEventListener("click", ()=> {
-    firstPageContainer.classList.add("display-none")
-    // searchField.style.display = "flex"
-    teamManagerPage.style.display = "block"
+startButton.addEventListener("click", (event)=> {
+    event.preventDefault();
+    teamNameInput.value = teamNameInput.querySelector(".search-input").value;
+    createTeamNameTag();
+    firstPageContainer.classList.add("display-none");
+    teamManagerPage.style.display = "block";
 })
+
+function createTeamNameTag() {
+    const teamTag = document.createElement("h4")
+    teamTag.innerText = teamNameInput.value
+    teamManagerPage.append(teamTag)
+}
 
 
